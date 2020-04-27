31fba268ba1034da6c63613774a692074e6ee665
marrato <15fram@caslab.queensu.ca>
2020-04-26 20:44:59 -0400

    Initial commit - Basic Backend and event handlers.

---
+++ app/src/main/java/com/example/fieldassetmanagement/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.fieldassetmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_SAP_URI = "com.example.fieldassetmanagement.EXTRA_SAP_URI";
+    public static final String EXTRA_SAP_ROW = "com.example.fieldassetmanagement.EXTRA_SAP_ROW";
+    public static final String EXTRA_SAP_FILENAME = "com.example.fieldassetmanagement.EXTRA_SAP_FILENAME";
+
+    private Uri csvURI;
+    private int row = 0;
+
+    private  static final int REQUEST = 69;
+
+    Button fileSelect;
+    Button openCSV;
+    TextView csvFileName, uriText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        openCSV = (Button) findViewById(R.id.openCSV);
+        fileSelect = (Button) findViewById(R.id.fileSelect);
+        uriText = (TextView) findViewById(R.id.uri);
+        csvFileName = (TextView) findViewById(R.id.csvFileDisplay);
+
+        openCSV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSingleAssetPage();
+            }
+        });
+
+        fileSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                activateSearch();
+            }
+        });
+    }
+
+    private void openSingleAssetPage() {
+        //TODO Check if a file has been selected
+        //TODO Chrashes when no file is selected
+        Uri singleAssetURI = csvURI;
+        String stringURI =  singleAssetURI.toString();
+        row = loadRowPreference(singleAssetURI);
+        Intent singleAssetIntent = new Intent(this, SingleAssetPage.class);
+        singleAssetIntent.putExtra(EXTRA_SAP_URI, stringURI);
+        singleAssetIntent.putExtra(EXTRA_SAP_ROW, row); //TODO get shared preference
+        singleAssetIntent.putExtra(EXTRA_SAP_FILENAME, getFileName(singleAssetURI));
+        startActivity(singleAssetIntent);
+    }
+
+    private void activateSearch() {
+
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("text/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQUEST);
+    }
+
+    @Override
+    protected  void onActivityResult(int request, int result, Intent data){
+        super.onActivityResult(request, result, data);
+
+        if(request == REQUEST && result == Activity.RESULT_OK){
+            if(data != null){
+                csvURI = data.getData();
+
+                Toast.makeText(this, "Uri: " + csvURI, Toast.LENGTH_LONG).show();
+                uriText.setText(csvURI.toString());
+                Toast.makeText(this, "Path: " + csvURI.getPath(), Toast.LENGTH_LONG).show();
+                csvFileName.setText(getFileName(csvURI));
+            }
+        }
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    public int loadRowPreference(Uri fileURI){
+        SharedPreferences rowPreference = getSharedPreferences(SingleAssetPage.ROW_PREFERENCES, MODE_PRIVATE);
+        int savedRow = rowPreference.getInt(getFileName(fileURI),1);
+        return savedRow;
+    }
+
+}
---
+++ app/src/main/java/com/example/fieldassetmanagement/SingleAssetPage.java
@@ -0,0 +1,152 @@
+package com.example.fieldassetmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.graphics.Path;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class SingleAssetPage extends AppCompatActivity implements OnItemSelectedListener{
+
+    private String fileName;
+    private List<String[]> curCSV;
+    private Uri csvURI;
+    private int row;
+
+    // Shared Preference Constants
+    public static final String ROW_PREFERENCES = "rowPres";
+
+    // All Spinner declarations
+    private Spinner fruitSpinner;
+
+    // All Spinner option declarations
+    private String[] fruitOptions;
+
+    TextView longitude, latitude, fruit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_asset_page);
+
+        Intent openIntent = getIntent();
+        String EXTRA_SAP_URI = openIntent.getStringExtra(MainActivity.EXTRA_SAP_URI);
+        int EXTRA_SAP_ROW = openIntent.getIntExtra(MainActivity.EXTRA_SAP_ROW, 1);
+        String EXTRA_SAP_FILENAME = openIntent.getStringExtra(MainActivity.EXTRA_SAP_FILENAME);
+        csvURI=Uri.parse(EXTRA_SAP_URI);    // Convert EXTRA_SAP_URI data back to a URI
+        fileName = EXTRA_SAP_FILENAME; // Get Short-form fileName from MainActivity
+        row = EXTRA_SAP_ROW; // Get loaded Shared Preference Row from MainActivity
+
+        // Load data from selected CSV file
+        try {
+            curCSV = loadCSVfromURI(csvURI);   // Load 2D arrayList from File
+            Toast.makeText(this, "" + fileName + " data loaded succesfully!", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) { // Throw FileNotFoundException if no file found
+            //TODO set curCSV to a blank arrayList so the program doesn't crash
+            Toast.makeText(this, "File Not Found Exception: " + fileName, Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "I/O Exception: " + fileName, Toast.LENGTH_LONG).show();
+
+        }
+        // Entry Headers
+        longitude = (TextView) findViewById(R.id.displayLongitude);
+        latitude = (TextView) findViewById(R.id.displayLatitude);
+        fruit = (TextView) findViewById(R.id.fruitHeader);
+
+        // Entry Spinners
+        fruitSpinner = (Spinner) findViewById(R.id.fruitEntry);
+
+        fruitSpinner.setOnItemSelectedListener(this);
+
+        row = 5;
+
+        // Spinner Options
+        fruitOptions = getSpinnerOptions(curCSV, 0);
+
+        // Spinner adapter
+        ArrayAdapter<String> sizeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, fruitOptions);
+
+        // Dropdown Spinner Style
+        sizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Attach adapter to spinner
+        fruitSpinner.setAdapter(sizeAdapter);
+        // Set Spinner selection to current value in curCSV data
+        fruitSpinner.setSelection(Arrays.asList(fruitOptions).indexOf(curCSV.get(row)[0]));
+        fruit.setText(curCSV.get(0)[0]);
+
+
+        longitude.setText(csvURI.toString());
+        latitude.setText(fileName);
+
+    }
+
+    // Spinner Listeners
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        // Save the new data to curCSV so that onSave it is changed
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
+
+
+    private List<String[]> loadCSVfromURI(Uri file) throws FileNotFoundException, IOException {
+
+        Scanner csvFileScanner = new Scanner(new BufferedReader(new InputStreamReader(getContentResolver().openInputStream(file))));
+
+        List<String[]> csvROW = new ArrayList<>();
+        int row = 0;
+        while(csvFileScanner.hasNextLine()){
+            String line = csvFileScanner.nextLine(); // Get single line from reader
+            String[] splitLine = line.split(",");
+            csvROW.add(row, splitLine);
+            row++;
+        }
+        return csvROW;
+    }
+
+    private String[] getSpinnerOptions(List<String[]> master, int column){
+        String[] allOptions = new String[master.size()-1]; // -1 to not include first descriptive row
+        //TODO add option for when an empty csv (without previous options) is loaded.
+        for(int i = 1; i < master.size(); i++){
+            allOptions[i-1]= master.get(i)[column];
+        }
+
+        List<String> spinnerOptions = Arrays.asList(allOptions).stream().distinct().collect(Collectors.<String>toList());
+
+        allOptions = new String[spinnerOptions.size()];
+        allOptions = spinnerOptions.toArray(allOptions);
+
+        return allOptions;
+    }
+
+    private void saveRowPreferences(){
+        SharedPreferences rowPreference = getSharedPreferences(ROW_PREFERENCES, MODE_PRIVATE);
+        SharedPreferences.Editor rowEditor = rowPreference.edit();
+        final String saveName = fileName;
+        rowEditor.putInt(saveName,row);
+    }
+}
